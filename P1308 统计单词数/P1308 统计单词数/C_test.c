66d78328b2964bc72566f9ad7ade536d53ccee88
卢信良 <1209425127@qq.com>
2018-11-29 13:23:04 +0800

    vs2017_1

---
+++ P1308 统计单词数/P1308 统计单词数/C_test.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)//禁止显示建议用scanf_s代替scanf
+#pragma warning(disable:6031)//禁止显示scanf返回值被忽略的报错
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
