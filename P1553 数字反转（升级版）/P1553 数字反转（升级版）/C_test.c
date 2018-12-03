36b6aff2355d5abfef0ab1f8d3d1642aab5d1324
卢信良 <1209425127@qq.com>
2018-12-03 20:08:41 +0800

    添加判断数组类型函数

--- P1553 数字反转（升级版）/P1553 数字反转（升级版）/C_test.c
+++ P1553 数字反转（升级版）/P1553 数字反转（升级版）/C_test.c
@@ -4,22 +4,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int TypeOFInput(char array[])//判断输入的数是什么类型，整数rt结尾位置，小数rt小数点位置，分数rt斜杠位置，百分数rt0
+int TypeOFInput(char array[])//判断输入的数是什么类型，整数rt sign*100+结尾位置，小数rt sign*100+小数点位置，分数rt sign*100+斜杠位置，百分数rt sign*100+结尾位置
 {
-	int i, sign = 0, lacate;//整数sign=0，小数sign=1，分数sign=2，百分数sign=3
+	int i, sign = 1, location;//整数sign=1，小数sign=2，分数sign=3，百分数sign=4
 
-	for (i = 0; i < 22 && array[i] != '/0'; i++)
+	for (i = 0; i < 22 && array[i] != '\0'; i++)
 	{
 		if (array[i] == '.')
 		{
-
+			sign = 2;
+			location = i;
 		}
+		else if (array[i] == '/')
+		{
+			sign = 3;
+			location = i;
+		}
+		else if (array[i] == '%')
+		{
+			sign = 4;
+			location = i;
+		}
+		else continue;
 	}
+	if (sign == 1) location = i - 1;
+
+	return sign * 100 + location;
 }
 
-int main()
+/*void reverse(char array[], int sign)
 {
 
+}
+*/
+int main()
+{
+	char array[22]= { '1', '2', '3', '5', '4' };
+	int a;
+
+	a = TypeOFInput(array);
+
+	printf("%d", a);
 
 	system("pause");
 	return 0;
